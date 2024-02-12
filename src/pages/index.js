2bf8e89da44aa5c6cc1e140e18d1f1210a47019d
Kausha3 <kaushatrivedi12@gmail.com>
2024-02-12 10:53:32 -0500

    Lab Activity

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,19 @@
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Profile from './components/profile'
+import ProfileMistake from './qcomps/profile_mistake'
+import FirstComp from './qcomps/firstcomp';
+import Bio from './qcomps/bios';
+import TodoList from './qcomps/todos';
 
 export default function Home() {
   return (
     <div className={styles.main}>
+      <ProfileMistake />
         <Profile />
+        <FirstComp />
+        <Bio />
+        <TodoList />
     </div>
   )
 }
